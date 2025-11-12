88a91488d850b0d8c18177423c0acb780ede5e5e
Niiaks <napappoe007@st.ug.edu.gh>
2025-11-12 19:27:03 +0000

    Add Nunito font support and update dependencies in app.json and package.json

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,8 +1,33 @@
-import { Slot } from "expo-router";
+import {
+  Nunito_400Regular,
+  Nunito_700Bold,
+  Nunito_900Black,
+} from "@expo-google-fonts/nunito";
+import { useFonts } from "expo-font";
+import { Slot, SplashScreen } from "expo-router";
+import { useEffect } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-
-// SplashScreen.preventAutoHideAsync()
+SplashScreen.preventAutoHideAsync();
 export default function RootLayout() {
+  const [fontsLoaded, fontError] = useFonts({
+    Nunito_400Regular,
+    Nunito_700Bold,
+    Nunito_900Black,
+  });
+
+  useEffect(() => {
+    if (fontError) {
+      console.error(fontError);
+    }
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded, fontError]);
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Slot />
