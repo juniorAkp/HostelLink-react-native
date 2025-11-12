b153991675b497570b11fc8c667444a6e532dc3f
Niiaks <napappoe007@st.ug.edu.gh>
2025-11-12 19:09:52 +0000

    Enhance index screen layout with new components and animations; add privacy statement link and styling.

--- src/app/(app)/(public)/index.tsx
+++ src/app/(app)/(public)/index.tsx
@@ -1,15 +1,91 @@
-import { Text, View } from "react-native";
+import { Linking, StyleSheet, Text, View } from "react-native";
+import Animated, { FadeInDown } from "react-native-reanimated";
+import AuthButton from "../../components/auth/AuthButton";
 
 export default function Index() {
+  const openWebBrowser = () => {
+    Linking.openURL("https://google.com");
+  };
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      {/* image view section*/}
+      <View style={styles.imageView}></View>
+      {/* text view section*/}
+      <View style={styles.contentView}>
+        <Text style={styles.text}>Welcome to HostelLink</Text>
+        <Animated.Text entering={FadeInDown}>
+          Find Hostels near you
+        </Animated.Text>
+        <View style={styles.buttonContainer}>
+          <Animated.View entering={FadeInDown.delay(100)}>
+            <AuthButton
+              title="Continue with Google"
+              onPress={() => {}}
+              color="#fff"
+              icon="logo-google"
+              buttonColor="#000"
+            />
+          </Animated.View>
+          <Animated.View entering={FadeInDown.delay(200)}>
+            <AuthButton
+              title="Continue with Apple"
+              onPress={() => {}}
+              color="#fff"
+              icon="logo-apple"
+              buttonColor="#000"
+            />
+          </Animated.View>
+        </View>
+        <Animated.View
+          style={styles.privacyContainer}
+          entering={FadeInDown.delay(400)}
+        >
+          <Text style={styles.privacyText}>
+            Please visit{" "}
+            <Text style={styles.privacyLink} onPress={openWebBrowser}>
+              HosteLink Privacy Statement
+            </Text>{" "}
+            to learn about personal data processing at HostelLink.
+          </Text>
+        </Animated.View>
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  imageView: {
+    flex: 0.8,
+  },
+  contentView: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#000",
+  },
+  buttonContainer: {
+    gap: 12,
+    width: "100%",
+  },
+  privacyContainer: {
+    marginTop: 30,
+    paddingHorizontal: 20,
+  },
+  privacyText: {
+    fontSize: 12,
+    color: "#999",
+    textAlign: "center",
+    lineHeight: 18,
+  },
+  privacyLink: {
+    color: "#4285F4",
+    textDecorationLine: "underline",
+  },
+});
---
+++ src/app/components/auth/AuthButton.tsx
@@ -0,0 +1,45 @@
+import Ionicons from "@expo/vector-icons/Ionicons";
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+
+interface AuthButtonProps {
+  title: string;
+  onPress: () => void;
+  color: string;
+  icon: keyof typeof Ionicons.glyphMap;
+  buttonColor: string;
+}
+const AuthButton = ({
+  title,
+  onPress,
+  color,
+  icon,
+  buttonColor,
+}: AuthButtonProps) => {
+  return (
+    <TouchableOpacity style={styles.authButton} onPress={onPress}>
+      <Ionicons
+        name={icon as keyof typeof Ionicons.glyphMap}
+        size={18}
+        color={color}
+      />
+      <Text style={styles.authButtonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+const styles = StyleSheet.create({
+  authButton: {
+    flexDirection: "row",
+    backgroundColor: "#4285F4",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 17,
+    borderRadius: 12,
+    gap: 4,
+  },
+  authButtonText: {
+    fontSize: 18,
+    color: "#fff",
+    fontWeight: "600",
+  },
+});
+export default AuthButton;
