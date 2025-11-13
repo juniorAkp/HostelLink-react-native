80e3a964dd3a143eab378e0a89db8a8cf6958f0e
Niiaks <napappoe007@st.ug.edu.gh>
2025-11-13 10:14:21 +0000

    Add login and registration methods to user store for authentication using Supabase

--- src/app/hooks/use-userStore.ts
+++ src/app/hooks/use-userStore.ts
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
 import type { profile as Profile } from "../data/profile";
+import { supabase } from "../lib/supabase";
 import zustandStorage from "../utils/zutstandStorage";
 
 interface UserStore {
@@ -8,6 +9,8 @@ interface UserStore {
   isGuest: boolean;
   setUser: (user: Profile) => void;
   setIsGuest: (isGuest: boolean) => void;
+  login: (email: string, password: string) => void;
+  register: (email: string, username: string, password: string) => void;
 }
 
 const useUserStore = create<UserStore>()(
@@ -17,6 +20,42 @@ const useUserStore = create<UserStore>()(
       user: null,
       setIsGuest: (isGuest: boolean) => set({ isGuest }),
       setUser: (user: Profile) => set({ user }),
+      login: async (email: string, password: string) => {
+        try {
+          const { data, error } = await supabase.auth.signInWithPassword({
+            email,
+            password,
+          });
+          if (error) {
+            throw error;
+          }
+          if (data?.user) {
+            set({ user: data.user as unknown as Profile, isGuest: false });
+            return data.user;
+          }
+          throw new Error("Invalid response from server");
+        } catch (err) {
+          throw err;
+        }
+      },
+      register: async (email: string, password: string) => {
+        try {
+          const { data, error } = await supabase.auth.signUp({
+            email,
+            password,
+          });
+          if (error) {
+            throw error;
+          }
+          if (data?.user) {
+            set({ user: data.user as unknown as Profile, isGuest: false });
+            return data.user;
+          }
+          throw new Error("Invalid response from server");
+        } catch (err) {
+          throw err;
+        }
+      },
     }),
     {
       name: "user",
