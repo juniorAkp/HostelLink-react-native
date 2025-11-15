95ae845019a9c913fbd36c6887b12a93830ee5c8
Niiaks <napappoe007@st.ug.edu.gh>
2025-11-15 07:24:18 +0000

    Implement search layout with customizable header options and add index screen for search functionality.

--- src/app/(app)/(auth)/(tabs)/search/_layout.tsx
+++ src/app/(app)/(auth)/(tabs)/search/_layout.tsx
@@ -1,8 +1,28 @@
+import { Fonts } from "@/src/app/constants/theme";
 import { Stack } from "expo-router";
 import React from "react";
 
 const Layout = () => {
-  return <Stack />;
+  return (
+    <Stack>
+      <Stack.Screen
+        name="index"
+        options={{
+          title: "Search",
+          headerSearchBarOptions: {
+            hideWhenScrolling: true,
+            placement: "automatic",
+            placeholder: "Search for hostels",
+            onChangeText: () => {},
+          },
+          headerBackTitleStyle: {
+            fontFamily: Fonts.brandBold,
+          },
+          navigationBarHidden: true,
+        }}
+      />
+    </Stack>
+  );
 };
 
 export default Layout;
---
+++ src/app/(app)/(auth)/(tabs)/search/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ScrollView, StyleSheet } from "react-native";
+
+const index = () => {
+  return <ScrollView>{/* Screen content */}</ScrollView>;
+};
+
+export default index;
+
+const styles = StyleSheet.create({});
