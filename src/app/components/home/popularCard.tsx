93d33d8fbb158653aa43fa2730e3b8d6ad11eb97
Niiaks <napappoe007@st.ug.edu.gh>
2025-11-17 22:24:18 +0000

    Refactor PopularCard component: remove useTheme hook and set static background

--- src/app/components/home/popularCard.tsx
+++ src/app/components/home/popularCard.tsx
@@ -8,7 +8,6 @@ import { moderateScale, scale, verticalScale } from "react-native-size-matters";
 import { blurhash } from "../../constants/blurhash";
 import { Fonts } from "../../constants/theme";
 import { Hostels } from "../../data/hostel";
-import { useTheme } from "../../hooks/useTheme";
 
 interface PopularCardProps {
   hostel: Hostels;
@@ -23,11 +22,10 @@ const PopularCard = ({
   onLike,
   isLiked = false,
 }: PopularCardProps) => {
-  const { colors } = useTheme();
   return (
     <Link href={`/hostel/${hostel.id}`} asChild>
       <TouchableOpacity
-        style={[styles.container, { backgroundColor: colors.card }]}
+        style={styles.container}
         onPress={onPress}
         activeOpacity={0.9}
       >
@@ -81,6 +79,7 @@ const styles = StyleSheet.create({
     width: scale(280),
     height: verticalScale(320),
     borderRadius: scale(16),
+    backgroundColor: "#fff",
     overflow: "hidden",
     marginRight: scale(16),
     shadowColor: "#000",
